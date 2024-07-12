0cc07c7c347e978adb4ff4c7abe340d145db75ad
Lucas Eduardo Zamora Sulub <lucasosulub2@gmail.com>
2024-07-12 10:39:00 -0500

    LZ_Correccion de errores dentro del index.tsx
    Se corrigio un error de versionamiento dentro del index.tsx el cual impledia la renderizacion de listado de datos, ya que  que la API ReactDOM.render ya no es compatible con React 18. En su lugar, se cambio a la API createRoot de ReactDOM.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Importa createRoot desde react-dom/client
 import { Provider } from 'react-redux';
 import store from './store/store'; // Asegúrate de importar tu store correctamente
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement); // Crea la raíz
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './store/store'; // Asegúrate de importar tu store correctamente
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
