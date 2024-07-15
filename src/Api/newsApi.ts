5628a06817f88a05d3333ba7247b825d5d0d223e
Lucas Eduardo Zamora Sulub <lucasosulub2@gmail.com>
2024-07-14 20:31:35 -0500

    LZ_Rediseño de la sección de noticias y optimizacion de codigo
    Se implemento un nuevo diseño a la sección de noticias ademas de implementarle el paginado para que pueda ser infinito las noticias y se realizo las configuraciones necesarias para que este optimizado

--- src/Api/newsApi.ts
+++ src/Api/newsApi.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const apiKey = 'b188b848fafd4f72b33f4ab4db33796d';
+const apiKey = '04d2d3b033964671b111571327ea7e55';
 const baseUrl = 'https://newsapi.org/v2';
 
-export const getVideoGameNews = async () => {
+export const getVideoGameNews = async (page = 1, pageSize = 9) => {
     try {
         const response = await axios.get(`${baseUrl}/everything`, {
             params: {
-                q: 'video game',
+                q: 'video games', // Consulta específica para videojuegos
                 apiKey: apiKey,
                 language: 'es',
                 sortBy: 'publishedAt',
-                pageSize: 10
+                page: page,
+                pageSize: pageSize
             }
         });
         return response.data.articles;
--- src/pages/News/NewsView.tsx
+++ src/pages/News/NewsView.tsx
@@ -1,62 +1,97 @@
-import React, { useEffect, useState, Suspense } from 'react';
-import { Grid, Typography, Box, CircularProgress } from '@mui/material';
-import { getVideoGameNews } from '../../Api/newsApi';
-import FooterView from '../../layout/Footer/FooterView';
-import Navegador from '../../layout/Navegador/Navegador';
-import { Article } from '../../Api/Types/index';
+import React, { useEffect, useState, useCallback, useRef, Suspense } from "react";
+import { Grid, Typography, Box, CircularProgress } from "@mui/material";
+import { getVideoGameNews } from "../../Api/newsApi";
+import FooterView from "../../layout/Footer/FooterView";
+import Navegador from "../../layout/Navegador/Navegador";
+import { Article } from "../../Api/Types/index";
 
-const NewsCard = React.lazy(() => import('../../Components/NewsGame/NewsCard'));
+const NewsCard = React.lazy(() => import("../../Components/NewsGame/NewsCard"));
 
 const NewsView: React.FC = () => {
-    const [articles, setArticles] = useState<Article[]>([]);
-    const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const observer = useRef<IntersectionObserver>();
 
-    useEffect(() => {
-        const fetchNews = async () => {
-            const news = await getVideoGameNews();
-            setArticles(news);
-            setLoading(false);
-        };
+  const fetchNews = useCallback(async (page: number) => {
+    setLoading(true);
+    const news = await getVideoGameNews(page);
+    setArticles((prevArticles) => [...prevArticles, ...news]);
+    setLoading(false);
+  }, []);
 
-        fetchNews();
-    }, []);
+  useEffect(() => {
+    fetchNews(page);
+  }, [page, fetchNews]);
 
-    return (
-        <div>
-            <Navegador />
-            <Typography
-                variant="h4"
-                sx={{
-                    textAlign: 'center',
-                    margin: '20px 0',
-                    fontFamily: 'TuTipografía',
-                    color: '#ffffff',
-                }}
-            >
-                Noticias Recientes de Videojuegos
-            </Typography>
-            <br />
-            <Suspense fallback={<CircularProgress />}>
-                <Grid container spacing={3}>
-                    {articles.map((article, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <NewsCard article={article} />
-                        </Grid>
-                    ))}
+  const lastArticleRef = useCallback((node: HTMLDivElement) => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [loading]);
+
+  return (
+    <div>
+      <Navegador />
+      <Box
+        sx={{
+          padding: { xs: 2, sm: 3, md: 4, lg: 8, xl: 10 },
+          pt: { xs: 2, sm: 3, md: 3, lg: 3, xl: 3 },
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: 45,
+            textAlign: "left",
+            marginBottom: "30px",
+          }}
+        >
+          Noticias de videojuegos
+        </Typography>
+        <br />
+        <Grid container spacing={3}>
+          {articles.map((article, index) => {
+            if (articles.length === index + 1) {
+              return (
+                <Grid item xs={12} sm={6} md={4} key={index} ref={lastArticleRef}>
+                  <Suspense fallback={<CircularProgress />}>
+                    <NewsCard article={article} />
+                  </Suspense>
                 </Grid>
-            </Suspense>
-            <Box
-                sx={{
-                    display: 'flex',
-                    gap: '1rem',
-                    marginTop: '1rem',
-                    justifyContent: 'center',
-                }}
-            >
-                {FooterView()}
-            </Box>
-        </div>
-    );
+              );
+            } else {
+              return (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Suspense fallback={<CircularProgress />}>
+                    <NewsCard article={article} />
+                  </Suspense>
+                </Grid>
+              );
+            }
+          })}
+        </Grid>
+        {loading && <CircularProgress />}
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "1rem",
+          marginTop: "1rem",
+          justifyContent: "center",
+        }}
+      >
+        {FooterView()}
+      </Box>
+    </div>
+  );
 };
 
 export default NewsView;
