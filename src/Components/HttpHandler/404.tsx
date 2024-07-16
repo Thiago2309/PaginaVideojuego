e686e40fdefeacb0d1faa11e781defd9627aabd1
Thiago2309 <118017482+Thiago2309@users.noreply.github.com>
2024-07-16 17:12:11 -0500

    Ajuste a la rama de feat-ajustes

--- src/Components/HttpHandler/404.tsx
+++ src/Components/HttpHandler/404.tsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import Footer from '../../layout/Footer/FooterView'
 import Navegador from '../../layout/Navegador/Navegador';
+import E404 from '../../assets/images/404.jpg';
+
 
 const Error404: React.FC = () => {
   const theme = useTheme();
@@ -26,15 +28,20 @@ const Error404: React.FC = () => {
       >
         <Card sx={{ backgroundColor: "#1C172B", width: "80%", mt: 2, mb: 2 }}>
           <CardContent sx={{ padding: isMobile ? 2 : 4 }}>
-            <Grid container alignItems="center" justifyContent="center">
+            <Grid container alignItems="center" justifyContent="center" direction="column">
               <Grid item xs={12}>
                 <Typography
                   variant="h4"
                   component="div"
                   sx={{ fontWeight: "bold", color: "#ffffff", textAlign: 'center', fontFamily: "'Press Start 2P', cursive" }}
                 >
-                  404
+                  ERROR 404
                 </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Box display="flex" justifyContent="center" mt={2}>
+                  <img src={E404} alt="Error 404" style={{ width: isMobile ? '100%' : '30%' }} />
+                </Box>
               </Grid>
             </Grid>
             <Typography
